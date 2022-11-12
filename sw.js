7a881e68f99ae581d2466606ffe3ecd8fbefcbc3
MarioJGalicia <118022740+MarioJGalicia@users.noreply.github.com>
2022-11-12 00:50:27 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,66 @@
-const STATIC_CACHE = "static-v1";
-
-const APP_SHELL = [
+;
+//asignar un nombre y versión al cache
+const CACHE_NAME = 'v1_cache',
+  urlsToCache = [
   "/",
   "index.html",
   "/css/style.css",
   "/css/styletwo.css",
   "/js/app.js",
   "/images/icons/icon-310x310.png",
-];
+  "https://fonts.googleapis.com/css?family=Oswald:200,700&display=swap",
+  "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css",
+  "https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css",
+  "https://code.jquery.com/jquery-3.3.1.slim.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js",
+  "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js",
+  ]
 
-self.addEventListener("install", (e) => {
-  const cacheStatic = caches
-    .open(STATIC_CACHE)
-    .then((cache) => cache.addAll(APP_SHELL));
-
-  e.waitUntil(cacheStatic);
-});
-
-self.addEventListener("fetch", (e) => {
-  console.log("fetch! ", e.request);
-  e.respondWith(
-    caches
-      .match(e.request)
-      .then((res) => {
-        return res || fetch(e.request);
+//durante la fase de instalación, generalmente se almacena en caché los activos estáticos
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache)
+          .then(() => self.skipWaiting())
       })
-      .catch(console.log)
-  );
-  //   e.waitUntil(response);
-});
+      .catch(err => console.log('Falló registro de cache', err))
+  )
+})
+
+//una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
+self.addEventListener('activate', e => {
+  const cacheWhitelist = [CACHE_NAME]
+
+  e.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            //Eliminamos lo que ya no se necesita en cache
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName)
+            }
+          })
+        )
+      })
+      // Le indica al SW activar el cache actual
+      .then(() => self.clients.claim())
+  )
+})
+
+//cuando el navegador recupera una url
+self.addEventListener('fetch', e => {
+  //Responder ya sea con el objeto en caché o continuar y buscar la url real
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => {
+        if (res) {
+          //recuperar del cache
+          return res
+        }
+        //recuperar de la petición a la url
+        return fetch(e.request)
+      })
+  )
+})
