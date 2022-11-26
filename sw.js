51762bacba0d6184d2f7f048d14d8160739372fe
MarioJGalicia <118022740+MarioJGalicia@users.noreply.github.com>
2022-11-26 00:13:55 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,68 @@
+;
+//asignar un nombre y versión al cache
+const CACHE_NAME = 'My_cache',
+  urlsToCache = [
+  "/",
+  "index.html",
+  "/css/segundo.css",
+  "/js/hero.js",
+  "/images/logos/ico-blue.svg",
+  "/images/logos/ico-pink.svg",
+  "/images/logos/ico-white.svg",
+  "/images/logos/innovatec.svg",
+  "https://fonts.googleapis.com/css?family=Oswald:200,700&display=swap",
+  "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css",
+  "https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css",
+  "https://code.jquery.com/jquery-3.3.1.slim.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js",
+  "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js",
+  ]
+
+//durante la fase de instalación, generalmente se almacena en caché los activos estáticos
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache)
+          .then(() => self.skipWaiting())
+      })
+      .catch(err => console.log('Falló registro de cache', err))
+  )
+})
+
+//una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
+self.addEventListener('activate', e => {
+  const cacheWhitelist = [CACHE_NAME]
+
+  e.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            //Eliminamos lo que ya no se necesita en cache
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName)
+            }
+          })
+        )
+      })
+      // Le indica al SW activar el cache actual
+      .then(() => self.clients.claim())
+  )
+})
+
+//cuando el navegador recupera una url
+self.addEventListener('fetch', e => {
+  //Responder ya sea con el objeto en caché o continuar y buscar la url real
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => {
+        if (res) {
+          //recuperar del cache
+          return res
+        }
+        //recuperar de la petición a la url
+        return fetch(e.request)
+      })
+  )
+})
